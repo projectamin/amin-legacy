3789c74c4c96fb16b57e4a578d4f748fca376adf
root <root@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2005-07-01 08:21:21 +0000

    does it work?
    git-svn-id: svn://projectamin.org/amin@7 145501bc-5ef2-0310-bc7e-edf69cb750f0

---
+++ NewTest.pm
@@ -0,0 +1,35 @@
+this is a test
+
+=head1 Name
+
+Amin::Test - test for the docs
+
+=head1 Example
+
+some example
+
+
+=head1 Description
+
+Amin::test is something
+
+
+=head1 Methods 
+
+=over 4
+
+=item *new
+   
+=back
+
+
+=head1 XML example
+
+	<amin:profile xmlns:amin="http://projectamin.org/ns/amin">
+		<amin:command name="mkdir">
+			<amin:param>/tmp/test_Dir</amin:param>
+		</amin:command>
+	</amin:profile>
+
+
+=cut
