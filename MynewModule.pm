5b92fe5467577bbab80f367e83bb67fa08201816
(no author) <(no author)@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2005-07-05 01:32:00 +0000

    test xml
    git-svn-id: svn://projectamin.org/amin@18 145501bc-5ef2-0310-bc7e-edf69cb750f0

---
+++ MynewModule.pm
@@ -0,0 +1,30 @@
+package SomePAckage;
+
+use strict;
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Some PAckage
+
+=head1 SYNOPSIS
+
+yea something here
+
+=head1 DESCRIPTION
+
+some description
+
+=head1 Methods 
+
+=over 4
+
+=item *some_methods
+
+=back
+
+=cut
