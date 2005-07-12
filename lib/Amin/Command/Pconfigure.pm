28f96fb614058074bdd7551d228d4843b7207440
swishy <swishy@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2005-07-12 07:50:41 +0000

    initial import of a special configure just for perl !!
    git-svn-id: svn://projectamin.org/amin@29 145501bc-5ef2-0310-bc7e-edf69cb750f0

---
+++ lib/Amin/Command/Pconfigure.pm
@@ -0,0 +1,176 @@
+package Amin::Command::Pconfigure;
+
+use strict;
+use vars qw(@ISA);
+use Amin::Elt;
+
+@ISA = qw(Amin::Elt);
+my %attrs;
+
+sub start_element {
+	my ($self, $element) = @_;
+	%attrs = %{$element->{Attributes}};
+	$self->attrs(%attrs);
+	if ($element->{LocalName} eq "command") {
+		$self->command($attrs{'{}name'}->{Value});
+	}
+	$self->element($element);
+	$self->SUPER::start_element($element);
+}
+
+sub characters {
+	my ($self, $chars) = @_;
+	my $data = $chars->{Data};
+	$data = $self->fix_text($data);
+	my $attrs = $self->{"ATTRS"};
+	my $element = $self->{"ELEMENT"};
+
+	if ($attrs{'{}name'}->{Value} eq "dir") {
+		if ($data ne "") {
+			$self->dir($data);
+		}
+	}
+	if ($attrs{'{}name'}->{Value} eq "env") {
+		if ($data ne "") {
+			$self->env_vars($data);
+		}
+	}
+	if ($element->{LocalName} eq "param") {
+		if ($attrs{'{}name'}->{Value} eq "") {
+			if ($data ne "") {
+				my @things = $data =~ m/([\*\+\.\w=\/-]+|'[^']+')\s*/g;
+				foreach (@things) {
+					$self->param($_);
+				}
+			}
+		}
+	}
+
+	if ($element->{LocalName} eq "flag") {
+		if ($attrs{'{}name'}->{Value} eq "") {
+			if ($data ne "") {
+				$self->flag(split(/\s+/, $data));
+			}
+		}
+	}
+	$self->SUPER::characters($chars);
+}
+
+sub end_element {
+	my ($self, $element) = @_;
+
+	if ($element->{LocalName} eq "command") {
+
+		my $dir = $self->{'DIR'};
+		my $xflag = $self->{'FLAG'};
+		my $param = $self->{'PARAM'};
+		my ($flag, @flag, @param);
+		my $log = $self->{Spec}->{Log};
+
+		foreach my $ip (@$xflag){
+			if (($ip =~ /^-/) || ($ip =~ /^--/)) {
+				push @flag, $ip;
+			} else {	
+				$flag = "--" . $ip;
+				push @flag, $flag;
+			}
+		}
+
+		foreach my $ip (@$param){
+			push @param, $ip;
+		}
+
+		if ($dir) {
+			if (! chdir $dir) {
+				$self->{Spec}->{amin_error} = "red";
+				my $text = "Unable to change directory to $dir. Reason: $!";
+				$self->text($text);
+
+				$log->error_message($text);
+				$self->SUPER::end_element($element);
+				return;
+			}
+		}
+
+		my %acmd;
+	        $acmd{'CMD'} = "./configure.gnu";
+		$acmd{'FLAG'} = \@flag;
+		$acmd{'PARAM'} = \@param;
+
+		if ($self->{'ENV_VARS'}) {
+			$acmd{'ENV_VARS'} = $self->{'ENV_VARS'};
+		}
+		
+		my ($name, $value);
+		
+		my $cmd = $self->amin_command(\%acmd);
+
+		if ($cmd->{STATUS} != 0) {
+			$self->{Spec}->{amin_error} = "red";
+			my $text = "Unable to execute configure.gnu in $dir. Reason: $cmd->{ERR}";
+			$self->text($text);
+
+			$log->error_message($text);
+			if ($cmd->{ERR}) {
+				$log->ERR_message($cmd->{ERR});
+			}
+			$self->SUPER::end_element($element);
+			return;
+		}
+
+		my $text = "Executing Configure ";
+		if (@flag) {
+			$text = $text . join (", ", @flag)
+		}
+		if (@param) {
+			$text = $text .  " " . join (", ", @param);
+		}
+		$text = $text . " in $dir";
+		$self->text($text);
+		$log->success_message($text);
+		if ($cmd->{OUT}) {
+			$log->OUT_message($cmd->{OUT});
+		}
+		$self->SUPER::end_element($element);
+	} else {
+		$self->SUPER::end_element($element);
+	}
+}
+
+sub config {
+	my $self = shift;
+	$self->{CONFIG} = shift if @_;
+	return $self->{CONFIG};
+}
+
+
+1;
+
+=head1 NAME
+
+Perl Configure - reader class filter for the Perl equivalent to the configure command.
+
+=head1 version
+
+
+=head1 DESCRIPTION
+
+  A reader class for the Perl equivalent to the configure command. 
+  
+=head1 XML
+
+=over 4
+
+=item Full example
+
+        <amin:command name="pconfigure">
+                <amin:flag>
+                        prefix=/usr
+                        -
+                </amin:flag>
+                <amin:shell name="dir">/usr/src/perl-5.8.6</amin:shell>
+        </amin:command>
+ 
+=back  
+
+=cut
