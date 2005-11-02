0363883acd2248378fa7daa7cd744324890d9136
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2005-11-02 19:06:34 +0000

    doubles
    git-svn-id: svn://projectamin.org/amin@127 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Command/Ln.pm
+++ lib/Amin/Command/Ln.pm
@@ -39,6 +39,23 @@ ln (coreutils) 5.0 March 2003
         </amin:command>
  </amin:profile>
 
+=item Double example
+ 
+ <amin:profile xmlns:amin='http://projectamin.org/ns/'>
+       <amin:command name="ln">
+                <amin:param name="source">original_thing</amin:param>
+                <amin:param name="target">linked_thing</amin:param>
+                <amin:flag>sf</amin:flag>
+                <amin:shell name="dir">/tmp/amin-tests/</amin:shell>
+        </amin:command>
+       <amin:command name="ln">
+                <amin:param name="source">original_thing</amin:param>
+                <amin:param name="target">linked_thing</amin:param>
+                <amin:flag>sf</amin:flag>
+                <amin:shell name="dir">/tmp/amin-tests2/</amin:shell>
+        </amin:command>
+ </amin:profile>
+
 =back  
 
 =cut
