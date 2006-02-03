804af702c5cb7c0ea5ea095fa79afcd6ea4e8808
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2006-02-03 13:05:17 +0000

    added license
    git-svn-id: svn://projectamin.org/amin@156 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Cond/Hostname.pm
+++ lib/Amin/Cond/Hostname.pm
@@ -1,5 +1,10 @@
 package Amin::Cond::Hostname;
 
+#LICENSE:
+
+#Please see the LICENSE file included with this distribution 
+#or see the following website http://projectamin.org.
+
 #Amin Hostname Conditional
 
 use strict;
