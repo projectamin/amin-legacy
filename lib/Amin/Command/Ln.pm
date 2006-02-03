bb566b07adfe2ed313c68133f5ce70c679e3a914
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2006-02-03 13:01:06 +0000

    added license
    git-svn-id: svn://projectamin.org/amin@154 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Command/Ln.pm
+++ lib/Amin/Command/Ln.pm
@@ -1,5 +1,10 @@
 package Amin::Command::Ln;
 
+#LICENSE:
+
+#Please see the LICENSE file included with this distribution 
+#or see the following website http://projectamin.org.
+
 use strict;
 use vars qw(@ISA);
 use Amin::Command::Link;
