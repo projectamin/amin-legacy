a5eb0f821f22e8965d93c7c7a471ba23f305c198
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2007-02-02 02:59:38 +0000

    fixed password issue, cleaned up
    git-svn-id: svn://projectamin.org/amin@254 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Protocol/Blowfish_SimpleAuth.pm
+++ lib/Amin/Protocol/Blowfish_SimpleAuth.pm
@@ -10,23 +10,15 @@ use Net::Daemon;
 use POSIX;
 use IPC::Run qw( run );
 use XML::SAX::ParserFactory;
-#use Amin::CLI::Login;
+use Amin::CLI::Login;
 use XML::SAX::Writer;
 use Crypt::Blowfish_PP;
-use IPC::Shareable (':lock');
-use Net::SMTP;
+#use IPC::Shareable (':lock');
+#use Net::SMTP;
 use IO::Socket;
 
 use vars qw(@ISA);
 @ISA = qw(Net::Daemon);
-
-
-#sub new {
-#	my $class = shift;
-#	my $self = {};
-#	bless($self, $class);	
-#	return $self;
-#}
 
 sub parse_uri {
 	my $self = shift;
@@ -41,29 +33,25 @@ sub parse_uri {
 		Type     => SOCK_STREAM)
 		or die "Couldn't connect to 
 		$networkmap->{'ip'}:$networkmap->{'port'} : $@\n";
-	
-	if ($self->{Spec}->{Param}) {
+	if ($self->{Spec}->{Filter_Param}) {
 		#decrypt
-		my $decrypter = Crypt::Blowfish_PP->new($self->{Spec}->{Param});
+		my $decrypter = Crypt::Blowfish_PP->new($self->{Spec}->{Filter_Param});
 		my $plainpass = $blowfish->decrypt($networkmap->{'password'});
 		my $plainkey = $blowfish->decrypt($networkmap->{'key'});
 		my $encryptor = Crypt::Blowfish_PP->new($plainkey);
-		$networkmap->{'password'} = $encryptor->encrypt($plainpass);
-		
+		my $chipertext = $encryptor->encrypt($plainpass);
+		$networkmap->{'password'} = pack("H*", $chipertext);
 	} else {
 		die "need the blowfish master key for your networkmap.";
-	}		
-my $text = (<<END);
-
+	}
+	my $text = (<<END);
 <amin:login xmlns:amin="http://projectamin.org/ns/">
 	<amin:uri>$uri</amin:uri>
         <amin:username>$networkmap->{'user'}</amin:username>
         <amin:password>$networkmap->{'password'}</amin:password>
 </amin:login>
-eof
 
 END
-
 	$socket->send($text);
 	my $newline;
 	while (1) {
@@ -77,65 +65,36 @@ END
 }
 
 sub Run ($) {
-
 	my($self) = @_;
 	my($line, $sock);
 	$sock = $self->{'socket'};
 	while (1) {
-
-		my (@line, $line);
-
-		#my $newline;
-		#while (1) {
-		#	if (!defined($line = $sock->getline())) {
-		#		last;
-		#	}
-		#	$_ =~ s/(^\s+|\s+$)//gm;
-		#	$line = $line . $_;
-			#$newline = $newline . $line; 
-		#}
-
-		while (<$sock>) {
-			if (/eof/) {
-			#if ($sock->eof) {
-				last;
+		#grab the $line from the $socket
+		if (!defined($line = $sock->getline())) {
+			if ($sock->error()) {
+				$self->Error("Client connection error %s", $sock->error());
 			}
-			$_ =~ s/(^\s+|\s+$)//gm;
-			$line = $line . $_;
+			$sock->close();
+			return;
 		}
-
-
-		#password is not loaded in Login.pm There are certain
-		#characters used in BlowFish encryption that bork out
-		#XML parsers. So we grab the Blowfish password manually
-		#and load it in $login
-		my ($beg, $rest) = split (/<amin:password>/, $line);
-		my ($passwd, $end) = split (/<\/amin:password>/, $rest);
-
-		#now let's delete password out of $line so the SAX parsers
-		#don't bork out....
-
-		$line = $beg . $end;
-
-		my $newprofile;
-		my $writer = XML::SAX::Writer->new(Output => \$newprofile);
+	
+	
 		my $handler = Amin::CLI::Login->new(Handler => $writer);
 		my $p = XML::SAX::ParserFactory->parser(Handler => $handler);
 		my $login = $p->parse_string($line);
 
-		#finally add passwd to %login
+		#decrypt password
+		
+		my $key = $self->{'key'};
+		my $blowfish = Crypt::Blowfish_PP->new($key);
+		my $crypttext =  unpack("H*", $login->{'passwd'});
+		my $passwd = $blowfish->decrypt($crypttext);
 		$login->{'passwd'} = $passwd;
 
-		if ($newprofile) {
-			$login->{'profile'} = $newprofile;
-		}
 
 		my ($sys_name, $sys_passwd, $sys_uid) = getpwnam($login->{'username'});
 
 		#authentication of the username and password
-		my $key = $self->{'key'};
-		my $blowfish = Crypt::Blowfish_PP->new($key);
-		my $passwd = $blowfish->decrypt($login->{'passwd'});
 
 		my $auth;
 		my $auth_type = $self->{'auth_type'};
@@ -218,6 +177,7 @@ sub Run ($) {
 					run \@cmd, \( $in, $out, $err );
 				}
 				$sock->send($out);
+				$sock->close();
 				return;
 			}
 		}
