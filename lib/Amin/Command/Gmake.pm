17bd6dda84d295f0556cd0cff1c8226de65314f4
swishy <swishy@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2007-05-25 11:13:56 +0000

    here goes another one just like the other.... just to keep fbsd happy :)
    git-svn-id: svn://projectamin.org/amin@306 145501bc-5ef2-0310-bc7e-edf69cb750f0

---
+++ lib/Amin/Command/Gmake.pm
@@ -0,0 +1,329 @@
+package Amin::Command::Ggmake;
+
+#LICENSE:
+
+#Please see the LICENSE file included with this distribution 
+#or see the following website http://projectamin.org.
+
+use strict;
+use warnings;
+use vars qw(@ISA);
+use Amin::Elt;
+
+@ISA = qw(Amin::Elt);
+my %attrs;
+
+sub start_element {
+	my ($self, $element) = @_;
+	%attrs = %{$element->{Attributes}};
+	if (!$attrs{'{}name'}->{'Value'}) {
+		$attrs{'{}name'}->{'Value'} = "";
+	}
+	$self->attrs(%attrs);
+	if (($element->{Prefix} eq "amin") && ($element->{LocalName} eq "command") && ($attrs{'{}name'}->{Value} eq "gmake")) {
+		$self->command($attrs{'{}name'}->{Value});
+	}
+	$self->element($element);
+	$self->SUPER::start_element($element);
+}
+
+sub characters {
+	my ($self, $chars) = @_;
+	my $data = $chars->{Data};
+	$data = $self->fix_text($data);
+	my $attrs = $self->attrs;
+	my $element = $self->{"ELEMENT"};
+	my $command = $self->command;
+	if (($command eq "gmake") && ($data ne "")) {
+		if ($element->{LocalName} eq "shell") {
+			if ($attrs{'{}name'}->{Value} eq "dir") {
+				$self->dir($data);
+			}
+			if ($attrs{'{}name'}->{Value} eq "env") {
+				$self->env_vars($data);
+			}
+		}
+
+
+		if ($element->{LocalName} eq "param") {
+			if ($attrs{'{}name'}->{Value} eq "") {
+				my @things = $data =~ m/([\+\.\w=\/-]+|'[^']+')\s*/g;
+				foreach (@things) {
+					$_ =~ s/(^'|'$)//gm;
+					$self->param($_);
+				}
+			}
+		}
+		if ($element->{LocalName} eq "flag") {
+			if ($attrs{'{}name'}->{Value} eq "") {
+				$self->flag(split(/\s+/, $data));
+			}
+		}
+	}
+	$self->SUPER::characters($chars);
+}
+
+sub end_element {
+	my ($self, $element) = @_;
+
+	if (($element->{LocalName} eq "command") &&  ($self->command eq "gmake")) {
+		my $dir = $self->{'DIR'};
+		my $param = $self->{'PARAM'};
+		my $xflag = $self->{'FLAG'};
+		my $command = $self->{'COMMAND'};
+		my $log = $self->{Spec}->{Log};
+
+		my ($flag, @flag, @param);
+
+		foreach my $ip (@$xflag){
+			if (!$ip) {next;};
+			if (($ip =~ /^-/) || ($ip =~ /^--/)) {
+				push @flag, $ip;
+			} else {	
+				$flag = "-" . $ip;
+				push @flag, $flag;
+			}
+		}
+
+		foreach my $ip (@$param){
+			push @param, $ip;
+		}
+
+		if ($dir) {
+			if (! chdir $dir) {
+				$self->{Spec}->{amin_error} = "red";
+				my $text = "Unable to change directory to $dir. Reason: $!";
+				$self->text($text);
+
+				$log->error_message($text);
+				$self->SUPER::end_element($element);
+				return;
+			}
+		}
+		
+		my %acmd;
+		$acmd{'CMD'} = $command;
+		$acmd{'FLAG'} = \@flag;
+		$acmd{'PARAM'} = \@param;
+
+		my ($name, $value);
+		if ($self->{'ENV_VARS'}) {
+			$acmd{'ENV_VARS'} = $self->{'ENV_VARS'};
+		}
+
+		my $cmd = $self->amin_command(\%acmd);
+		if ($cmd->{STATUS} != 0) {
+			$self->{Spec}->{amin_error} = "red";
+			my $text = "Unable to execute $command in $dir. Reason: $cmd->{ERR}";
+			$self->text($text);
+
+			$log->error_message($text);
+			if ($cmd->{ERR}) {
+				$log->ERR_message($cmd->{ERR});
+			}
+			$self->SUPER::end_element($element);
+			return;
+		}
+
+		my $text = "Executing $command " . join (" ", @param) . " in $dir";
+		if ($self->{'ENV_NAME'}) {
+			$text = $text . " with enviroment settings of $self->{'ENV_NAME'}=$self->{'ENV_VALUE'}";
+		}
+		$self->text($text);
+		$log->success_message($text);
+		if ($cmd->{OUT}) {
+			$log->OUT_message($cmd->{OUT});
+		}
+		#reset this command
+		
+		$self->{DIR} = undef;
+		$self->{FLAG} = [];
+		$self->{PARAM} = [];
+		$self->{COMMAND} = undef;
+		$self->{ATTRS} = undef;
+		$self->{ENV_VARS} = [];
+		$self->{ELEMENT} = undef;
+		$self->SUPER::end_element($element);
+	} else {
+		$self->SUPER::end_element($element);
+	}
+}
+
+
+sub filter_map {
+	my $self = shift;
+	my $command = shift;
+	my %command;
+	my @flags;
+	my @params;
+	my @shells;
+	my @things = split(/([\*\+\.\w=\/-]+|'[^']+')\s*/, $command);
+
+	my %scratch;
+	my $stop;
+	foreach (@things) {
+	#check for real stuff
+	if ($_) {
+		#check for flag
+		if (($_ =~ /^-.*$/) || ($_ =~ /^--.*$/) || ($scratch{name})) {
+			#it is a flag
+			my %flag;
+			my $char;
+			$_ =~ s/-//;
+			$_ =~ s/--//;
+			if ($scratch{name}) {
+				#this completes the -m 0755 crap
+				if ($_ =~ /\d+/) {
+					$char = $_;
+				} else {
+					#this is a param and their -m is 0000
+					#why they want this is unknown :)
+					my %param;
+					$param{"char"} = $_;
+					push @params, \%param;
+				}
+					
+				
+				
+				$_ = $scratch{name};
+				#undefine stuff
+				$stop = undef;
+				%scratch = {};
+			} else {
+				if ($_ =~ /^.*=.*$/) {
+					#check for stuff like -m=0755 crap
+					($_, $char) = split (/=/, $_);
+				} elsif ($_ eq "C") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "f") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "I") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "j") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "l") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "o") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} elsif ($_ eq "W") {
+					#check for stuff like -m 0755 crap
+					$scratch{name} = $_;
+					$stop = 1;
+				} else  {
+					#its just a flag
+					$char = $_;
+					$_ = undef;
+				}
+			}
+			
+			if (!$stop) {
+				if ($_) {
+					$flag{"name"} = $_;
+				}
+			
+				$flag{"char"} = $char;
+				push @flags, \%flag;
+			}
+		
+		} elsif ($_ =~ /^.*=.*$/) {
+			my %shell;
+			#it is an env variable 
+			$shell{"name"} = 'env';
+			$shell{"char"} = $_;
+			push @shells, \%shell;
+		} else {
+			#it is either a param, command name
+			if (!$command{name}) {
+				$command{name} = $_;
+			} else {
+				my %param;
+				$param{"char"} = $_;
+				push @params, \%param;
+			}
+		}
+	}
+	}
+	
+	if (@shells) {
+		$command{shell} = \@shells;
+	}
+	if (@flags) {
+		$command{flag} = \@flags;
+	}
+	if (@params) {
+		$command{param} = \@params;
+	}
+	
+	my %fcommand;
+	$fcommand{command} = \%command;
+	return \%fcommand;	
+}
+
+sub version {
+	return "1.0";
+}
+
+1;
+
+=head1 NAME
+
+Make - reader class filter for the gmake command.
+
+=head1 version
+
+GNU 22 August 1989	
+
+=head1 DESCRIPTION
+
+  A reader class for the gmake command. 
+  
+=head1 XML
+
+=over 4
+
+=item Full example
+
+ <amin:profile xmlns:amin='http://projectamin.org/ns/'>
+	<amin:command name="gmake">
+		<amin:shell name="dir">/tmp/amin-tests/fake-0.01</amin:shell>
+	</amin:command>
+	<amin:command name="gmake">
+		<amin:param>install</amin:param>
+		<amin:shell name="dir">/tmp/amin-tests/fake-0.01</amin:shell>
+	</amin:command>
+ </amin:profile>
+
+=item Double example
+ 
+ <amin:profile xmlns:amin='http://projectamin.org/ns/'>
+	<amin:command name="gmake">
+		<amin:shell name="dir">/tmp/amin-tests/fake-0.01</amin:shell>
+	</amin:command>
+	<amin:command name="gmake">
+		<amin:param>install</amin:param>
+		<amin:shell name="dir">/tmp/amin-tests/fake-0.01</amin:shell>
+	</amin:command>
+	<amin:command name="gmake">
+		<amin:shell name="dir">/tmp/amin-tests2/fake-0.01</amin:shell>
+	</amin:command>
+	<amin:command name="gmake">
+		<amin:param>install</amin:param>
+		<amin:shell name="dir">/tmp/amin-tests2/fake-0.01</amin:shell>
+	</amin:command>
+ </amin:profile>
+
+=back  
+
+=cut
