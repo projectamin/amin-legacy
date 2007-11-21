f3991e7058bd97e7e9ac6301a7b644b3c29557a9
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2007-11-21 08:33:30 +0000

    new version initial import
    git-svn-id: svn://projectamin.org/amin@345 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Protocol/Standard.pm
+++ lib/Amin/Protocol/Standard.pm
@@ -6,117 +6,109 @@ package Amin::Protocol::Standard;
 #or see the following website http://projectamin.org.
 
 use strict;
-use Net::Daemon;
 use Amin;
 use LWP::UserAgent;
 use Amin::Protocol::Datastore;
 use Amin::Machine::Filter::XInclude;
 use XML::SAX::PurePerl;
 use Digest::MD5;
+use IO::All;
+use Amin::Uri;
 use IO::Socket;
 
-use vars qw(@ISA);
-@ISA = qw(Net::Daemon);
-
+sub new {
+	my $class = shift;
+	my %args = @_;
+	my $self;
+	$self = bless \%args, $class;
+	return $self;
+}
 
 sub parse_uri {
 	my $self = shift;
 	my $networkmap = shift;
 	my $uri = shift;
-	my $line;
-	#setup the socket infos
-	my $ip = $networkmap->{'ip'};
-	my $port = $networkmap->{'port'};
-	my $socket = IO::Socket::INET->new(PeerAddr => $ip,
-		PeerPort => $port,
-		Proto    => "tcp",
-		Type     => SOCK_STREAM)
-		or die "Couldn't connect to 
-		$networkmap->{'ip'}:$networkmap->{'port'} : $@\n";
-	#send the uri to the other side
-	$socket->send($uri);
-	#get back the response and return it
-	my $newline;
-	while (1) {
-		if (!defined($line = $socket->getline())) {
-			last;
-		}
-		$newline = $newline . $line;
-	}
-	close($socket);
-	return $newline;
+	my $socket = IO::Socket::INET->new(PeerAddr => $networkmap->{'ip'},
+                                PeerPort => $networkmap->{'port'},
+                                Proto    => "tcp",
+                                Type     => SOCK_STREAM)
+	or die "Couldn't connect to $networkmap->{'ip'}:$networkmap->{'port'} : $@\n";
+	print $socket $uri;
+	my $output = <$socket>;
+	close($socket);	
+	return $output;
 }
 
-sub Run ($) {
-	my($self) = @_;
-	my($line, $sock);
-	$sock = $self->{'socket'};
-	while (1) {
-		#grab the $line from the $socket
-		if (!defined($line = $sock->getline())) {
-			if ($sock->error()) {
-				$self->Error("Client connection error %s", $sock->error());
-			}
-			$sock->close();
-			return;
-		}
-		#fork a child
-		my $pid = fork;
-		if ($pid eq 0) {
-			wait;
-			#should this be return; instead?
-			#is this the defunct process problem?
-			#
-			exit;
-			return;
+sub Run {
+	my $self = shift;
+	my $info = "$self->{Ip}:$self->{Port}";
+	my $server = IO::Socket::INET->new(
+					LocalAddr => $info,
+					Type => SOCK_STREAM,
+					Reuse => 1,
+					Listen => 1,
+					 )
+	or die "Could not bind as a server on ip $self->{Ip} port $self->{Port} : $@\n";
+
+	while (my $client = $server->accept()) {
+		next if my $pid = fork;
+		die "fork: $!" unless defined $pid;
+		my $uri = $client->getline();
+		#this is a simple amin controller.
+		#it takes the $uri supplied, grabs the
+		#resulting profile, checksums the uri/profile
+		#compares that checksum to it's internal datastore
+		#of checksums, and if matches parses as whatever
+		#user this daemon runs as.
+		my $aout;
+		my $m = Amin->new ();
+		#grab profile
+		my $ua = LWP::UserAgent->new;
+		my $req = HTTP::Request->new(GET => $uri);
+		my $res = $ua->request($req);
+		if ($res->is_success) {
+			$aout = $res->content;
 		} else {
-			#this is a simple amin controller.
-			#it takes the $uri supplied, grabs the
-			#resulting profile, checksums the uri/profile
-			#compares that checksum to it's internal datastore
-			#of checksums, and if matches parses as whatever
-			#user this daemon runs as.
-			my $m = Amin->new ();
-			my $lout;
-			#grab profile
-			my $ua = LWP::UserAgent->new;
-			my $aout;
-			#change?
-			my $uri = $line;
-			my $req = HTTP::Request->new(GET => $uri);
-			my $res = $ua->request($req);
-			if ($res->is_success) {
-				$aout = $res->as_string;
-			} else {
-				$aout .= " Unable to download $uri.";
-			
-			}
-			if ($aout = $uri->is_uri($aout)) {
-			
-				#checksum the profile
-				my $md5 = Digest::MD5->new;
-				$md5->add($aout);
-				my $digest = $md5->hexdigest;
-				#compare to datastore of checksums
-				my $ds = $self->{Data_Store};
-				my $h = Amin::Protocol::Datastore->new();
-				my $x = Amin::Machine::Filter::XInclude->new(Handler => $h);
-				my $p = XML::SAX::PurePerl->new(Handler => $x);
-				$ds = $p->parse_uri($ds);	
-				#if checksum matches or not.
-				foreach (keys %$ds) {
-					if ($digest eq $ds->{$_}->{checksum}) {
-						$aout = $m->parse_uri($uri);
-					} else {
-						$aout = "sorry this profile is not allowed";
-					}
+			$aout .= " Unable to download $uri.";
+		}
+		my $uric = Amin::Uri->new();
+		if ($uric->is_uri($uri)) {
+			#checksum the profile
+			my $md5 = Digest::MD5->new;
+			$md5->add($aout);
+			my $digest = $md5->hexdigest;
+			#compare to datastore of checksums
+			my $ds = $self->{Data_Store};
+			my $h = Amin::Protocol::Datastore->new();
+			my $x = Amin::Machine::Filter::XInclude->new(Handler => $h);
+			my $p = XML::SAX::PurePerl->new(Handler => $x);
+			$ds = $p->parse_uri($ds);	
+			#if checksum matches or not.
+			foreach (keys %$ds) {
+				if ($digest eq $ds->{$_}->{checksum}) {
+					$aout = $m->parse_uri($uri);
+				} else {
+					$aout = "sorry this profile is not allowed";
 				}
 			}
-			$sock->send($aout);
-			$sock->close();
-			return;
 		}
+		print $client $aout;
+		$client->shutdown(2);
+		#close($client); 
+		exit;
 	}
+	$SIG{CHLD} = \&REAPER;
+	$server->shutdown(2);
+}
+
+
+
+# set up the socket SERVER, bind and listen ...
+use POSIX qw(:sys_wait_h);
+
+sub REAPER {
+    1 until (-1 == waitpid(-1, WNOHANG));
+    $SIG{CHLD} = \&REAPER;                 # unless $] >= 5.002
 }
 
 1;
