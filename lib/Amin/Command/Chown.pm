5e1d7783f041d156471b7d9b5c8773c557394ec6
bdumm <bdumm@145501bc-5ef2-0310-bc7e-edf69cb750f0>
2007-12-16 00:00:37 +0000

    removed extra
    git-svn-id: svn://projectamin.org/amin@410 145501bc-5ef2-0310-bc7e-edf69cb750f0

--- lib/Amin/Command/Chown.pm
+++ lib/Amin/Command/Chown.pm
@@ -165,7 +165,6 @@ sub end_element {
 
 		my $cmd = $self->amin_command(\%acmd);
 
-		my $default = "0"; #setup the default msg flag
 		if ($cmd->{TYPE} eq "error") {
 			$self->{Spec}->{amin_error} = "red";
 			my $text = "Chown Failed for $ogroup, on $target. Reason: $cmd->{ERR}";
